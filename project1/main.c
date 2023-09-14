a6ca355b22bc574e9d221122c74d5950e7e8cdf1
Seth Wolfgang <sethwolfgang@pop-os.localdomain>
2023-09-14 09:19:15 -0400

    lab3 initial commit

--- project1/main.c
+++ project1/main.c
@@ -1,11 +1,48 @@
+#include "main.h"
+
+
+typedef struct {
+    char* message;
+    int receiver;
+} apple;
+
+
+int main(int argc, char** argv) {
+
+    int fd[3], pid, outputLength;
+    int pipeCreationsResult = pipe(fd);
+
+    if (pipeCreationsResult < 0) {
+        perror("Failed pipe creation\n");
+        exit(1);
+    }
+
+    pid = fork();
+
+    // spawning the circle
+    int processCounter = 2; // set to 2 because the child needs to spawn the fork, so 2 children already exist
+    while (processCounter < 3 && pid == 0) {
+        if ((pid = fork()) < 0) {
+            perror("Failed Child creation on child: %d\n", processCounter);
+            exit(1);
+        }
+    }
+
+    // if child
+    if (pid == 0) {
+        write(fd[1], output, outputLength);
+        printf("Child wrote: [%s] to fd[]\n", output);
+        free(output);
+    }
+    else { // parent
+        read(fd[0], input, outputLength);
+        printf("Parent receieved: [%s] from \n", input);
+        free(input);
+    }
 
 
 
-
-
-int main(int argc, char** argv){
-
-    
+    return 0;
 
 }
 
