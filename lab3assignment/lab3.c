00df7003b4d6a7126d8baa4cbc0cede33ec55b5c
Seth Wolfgang <sethwolfgang@pop-os.localdomain>
2023-09-14 09:47:01 -0400

    added boilerplate

--- lab3assignment/lab3.c
+++ lab3assignment/lab3.c
@@ -1,9 +1,36 @@
 
+
+void signalHandlerSIGUSR1();
+void signalHandlerSIGUSR2();
+
 int main(int argc, char** argv){
+    signal(SIGUSR1, signalHandlerSIGUSR1);
+    signal(SIGUSR2, signalHandlerSIGUSR2);
+
+    int pid;
+
+    pid = fork();
 
 
-
-
+    if(pid < 0){
+        perror("Failed to fork");
+        exit(1);
+    }
+    if(pid == 0){
+        exit(0)
+    }
 
     return 0;
+}
+
+
+void signalHandlerSIGUSR1(){
+
+
+}
+
+
+void signalHandlerSIGUSR2(){
+
+
 }
