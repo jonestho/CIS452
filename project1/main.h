01a046fb3f4c6a6252e8935ba3b8905353716d4b
Seth Wolfgang <sethwolfgang@pop-os.localdomain>
2023-09-20 12:42:22 -0400

    working but need an elegant way to kill processes

--- project1/main.h
+++ project1/main.h
@@ -2,3 +2,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
+
+typedef struct {
+    int ID;
+    int receiver;
+    int readPipe;
+    int writePipe;
+    char message[500];
+    int sent;
+} Apple;
+
+
+
+void getUserInput(int* numChildren, char* message, int* dest);
+Apple appleFactory(int ID, int receiver, int readPipe, int writePipe, char* message);
+void handleSignal(int sig);
