edc82e10e1ce0cc780af875212dc4605fc72c17a
Thomas <jonestho@mail.gvsu.edu>
2023-09-25 19:29:00 -0400

    multithread program with arg bug

--- Lab4/multiThreadServer.c
+++ Lab4/multiThreadServer.c
@@ -11,25 +11,29 @@ void *doDispatch(void *arg);
 void *doWork(void *arg);
 
 int filesAccessed = 0;
+int workersRunning = 0;
 
-int main(int argc, int argv[]){
+int main(int argc, int argv[])
+{
     signal(SIGINT, signalHandler);
 
     pthread_t dispatch;
     void *dispatchResult;
-    
+
     int dispatchStatus;
     int dispatchJoinResult;
     char fileName[500];
 
     dispatchStatus = pthread_create(&dispatch, NULL, doDispatch, &fileName);
-    if(dispatchStatus != 0){
+    if (dispatchStatus != 0)
+    {
         fprintf(stderr, "Thread create error %d: %s\n", dispatchStatus, strerror(dispatchStatus));
         exit(1);
     }
 
     dispatchJoinResult = pthread_join(dispatch, &dispatchResult);
-    if(dispatchJoinResult != 0 ){
+    if (dispatchJoinResult != 0)
+    {
         fprintf(stderr, "Join error %d: %s\n", dispatchJoinResult, strerror(dispatchJoinResult));
         exit(1);
     }
@@ -37,8 +41,10 @@ int main(int argc, int argv[]){
     return 0;
 }
 
-void *doDispatch(void *arg){
-    while(1){
+void *doDispatch(void *arg)
+{
+    while (1)
+    {
         pthread_t worker;
         void *workerResult;
 
@@ -48,34 +54,39 @@ void *doDispatch(void *arg){
         printf("Enter a filename: ");
         scanf("%s", arg);
 
+        printf("Searching...\n");
         workerStatus = pthread_create(&worker, NULL, doWork, arg);
-        if(workerStatus != 0){
+        if (workerStatus != 0)
+        {
             fprintf(stderr, "Thread create error %d: %s\n", workerStatus, strerror(workerStatus));
             exit(1);
         }
-
-        workerJoinResult = pthread_join(worker, &workerResult);
-        if(workerJoinResult != 0 ){
-            fprintf(stderr, "Join error %d: %s\n", workerJoinResult, strerror(workerJoinResult));
-            exit(1);
-        }
+        workersRunning++;
     }
 
-    return (void *) arg;
+    return (void *)arg;
 }
 
-void *doWork(void *arg){
-    printf("Searching...\n");
-    if ((double) rand() / (double)RAND_MAX >= 0.2)
+void *doWork(void *arg)
+{
+    if ((double)rand() / (double)RAND_MAX >= 0.2)
         sleep(1);
     else
         sleep(rand() % 3 + 7);
 
-    printf("File \"%s\" found. Terminating thread\n", arg);
+    printf("\nFile \"%s\" found. Terminating thread\n", arg);
+    
     filesAccessed++;
+    workersRunning--;
 }
 
-void signalHandler(int signal){
+void signalHandler(int signal)
+{   
+    if(workersRunning != 0){
+        printf("\nWaiting for searches to finish.\n");
+        while(workersRunning != 0);
+    }
+
     printf("\nFiles Accessed: (%d) Exiting program.\n", filesAccessed);
     exit(0);
 }
