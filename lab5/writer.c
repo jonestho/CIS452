72f42053f32a6ecc08b141613c5442c5e26e7bc6
Seth Wolfgang <sethwolfgang@pop-os.localdomain>
2023-09-29 20:20:13 -0400

    writer is written, but untested

--- lab5/writer.c
+++ lab5/writer.c
@@ -1,24 +1,114 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <sys/ipc.h>
+#include <stdlib.h>
+#include <sys/shm.h>
 
+void detach();
+void removeSegment();
+void attach();
+void quitSignalHandler(int sig);
+void getUserInput(char* input);
 
+int shmId;
+char* sharedMemoryPtr;
+
+#define SM_SIZE 500
 
 int main(int argc, char** argv) {
-    int ID = atoi(argv[1]);
+    signal(SIGINT, quitSignalHandler);
+    char* keyPath = ".key";
+    key_t key = ftok(argv[0], 7);
+    if (key < 0) {
+        perror("Unable to get key\n");
+        exit(1);
+    }
 
-    //make a while loop to read using ftok()
+    uint8_t* sharedMemoryPtr, *readFlag1, *readFlag2;
+    char* stringStart;
+    char* userInput = (char*)malloc(sizeof(char) * SM_SIZE - 3);
+
+    // Creates the shared memory segment
+    if ((shmId = shmget(key, SM_SIZE, IPC_CREAT | S_IRUSR | S_IWUSR)) < 0) {
+        perror("Unable to get shared memory\n");
+        exit(1);
+    }
+    attach();
+
     /*
-                key_t ftok(const char *pathname, int proj_id);
-                https://man7.org/linux/man-pages/man3/ftok.3.html
+        Sets pointers to where they need to point
     
-
+        |WRITE_FLAG | READ_FLAG1 | READ_FLAG2 | STRING_START....|
+    
     */
+    readFlag1 = (uint8_t*)sharedMemoryPtr + 1;
+    readFlag2 = (uint8_t*)sharedMemoryPtr + 2;
+    stringStart = (uint8_t*)sharedMemoryPtr + 3; // Start of string
 
+    // Set initial values
+    *sharedMemoryPtr = 0; // Write flag
+    *readFlag1 = 0; // Read flag 1
+    *readFlag2 = 0; // Read flag 2
 
-   
+    while (1) {
+        getUserInput(userInput);
 
-    char* keyPath = ".key"
-    key_t key = ftok(keyPath, 7);
+        // Write to shared memory
+        memcpy(stringStart, userInput, strlen(userInput));
+        *sharedMemoryPtr = 1;
 
+        while (*readFlag1 != 1 || *readFlag2 != 1);
 
+        *readFlag1 = 0;
+        *readFlag2 = 0;
+    }
+
+    
+    printf("Value a: %p\t Value b: %p\n", (void*)sharedMemoryPtr, (void*)sharedMemoryPtr + SM_SIZE);
+    printf("ID: %d\n", shmId);
 
     return 0;
+}
+
+
+void detach() {
+    if (shmdt(sharedMemoryPtr) < 0) {
+        perror("Unable to detach\n");
+        exit(1);
+    }
+}
+
+void removeSegment() {
+    if (shmctl(shmId, IPC_RMID, 0) < 0) {
+        perror("Unable to deallocate\n");
+        exit(1);
+    }
+}
+
+void attach() {
+    if ((sharedMemoryPtr = shmat(shmId, 0, 0)) == (void*)-1) {
+        perror("Unable to attach\n");
+        exit(1);
+    }
+}
+
+void quitSignalHandler(int sig) {
+    *sharedMemoryPtr = 2;
+    sleep(1);
+    detach();
+    removeSegment();
+    exit(0);
+}
+
+void getUserInput(char* input) {
+    printf("Enter a string: ");
+    fgets(input, SM_SIZE - 3, stdin);
 }
