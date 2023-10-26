6daaeda64998b27bdb7aa583334dc2fde8297bbf
Thomas <jonestho@mail.gvsu.edu>
2023-10-26 09:03:08 -0400

    initialize semaphores

--- Project2/kitchen.c
+++ Project2/kitchen.c
@@ -1,3 +1,4 @@
+#include <pthread.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,8 +7,20 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/shm.h>
+#include <sys/sem.h>
 
-int main(int argc, char **argv){
+struct sembuf p = {0, -1, SEM_UNDO}; // wait
+struct sembuf v = {0, +1, SEM_UNDO}; // signal
+
+int main(int argc, char **argv)
+{
+    int sharedMemoryID;
+    int utensilSemID, pantrySemID, fridgeSemID, ovenSemID; // utensilSemID is for mixers, bowls, and spoons.
+
+    key_t keySHM = ftok("kitchen.c", 0);
+    key_t keySEM = ftok("baker.c", 1);
+
     int numOfBakers;
     char* userInput = (char*) malloc(sizeof(char) * 10); 
 
@@ -15,7 +28,10 @@ int main(int argc, char **argv){
     fgets(userInput, sizeof(char) * 10, stdin);
 
     numOfBakers = atoi(userInput);
-
     printf("You have entered %d bakers.\n", numOfBakers);
+
+
+
+    free(userInput);
     return 0;
 }
