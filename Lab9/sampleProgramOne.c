35b5e7d4a0613c49d2802eae328a49b1c0770568
Seth Wolfgang <sethwolfgang@pop-os.localdomain>
2023-11-07 17:49:36 -0500

    adding corrected sampleProgramOne.c

--- Lab9/sampleProgramOne.c
+++ Lab9/sampleProgramOne.c
@@ -2,19 +2,21 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define SIZE 16
-
-int main()
-{
-    char *data1;
-    data1 = malloc(SIZE);
-
-    char *data1Pointer = &data1;
+int main() {
+    char* data1;
+    size_t size = 16;
+    data1 = (char*)malloc(sizeof(char) * 16);
 
     printf("Please input username: ");
-    scanf("%s", data1);
+
+    // gets the line from user and automatically allocates memory
+    int lineSize = getline(&data1, &size, stdin); 
+
+    // remove the \n from when you press 'enter'
+    memset(&data1[lineSize-1], '\0', 1); 
 
     printf("You entered: [%s]\n", data1);
     free(data1);
+
     return 0;
 }
