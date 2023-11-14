c58b3bbca26307f1feb8c8d12229a6fbd26ed432
Thomas <jonestho@mail.gvsu.edu>
2023-11-13 23:24:18 -0500

    lab10 work

--- Lab10/sampleProgramOne.c
+++ Lab10/sampleProgramOne.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define COEFFICIENT 63
+#define COEFFICIENT 64
 #define KB 1024
 #define LOOP 1
 
@@ -9,6 +9,12 @@ int main(){
     int count, *intPtr;
     long int i, j, dim = COEFFICIENT * KB;
 
+    long int memory = 16435429376;
+    long int swap = 2147479552;
+    long int cap = memory + swap;
+
+    long int projectedMemory = dim * dim * sizeof(int);
+    
     if ((intPtr = malloc ((dim * dim * sizeof(int)))) == 0) 
     {
         perror ("totally out of space");
@@ -20,5 +26,5 @@ int main(){
             for (j=0; j<dim; j++)
                 intPtr[i * dim + j] = (i + j) % count;
                 free (intPtr);
-                return 0;            
+                return 0;       
 }
