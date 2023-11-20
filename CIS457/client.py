183235a3db4fd649fac74204ce3114662074a082
Thomas <jonestho@mail.gvsu.edu>
2023-11-19 21:15:38 -0500

    create threads for client

--- CIS457/client.py
+++ CIS457/client.py
@@ -1,17 +1,14 @@
 import random
 import signal
 import socket
-import sys
+import threading
 import time
 
-interruptReceived = False
 
-def interruptHandler(sigNum, signalFrame):
+def signalHandler(sigNum, sigHandler):
     global interruptReceived
     interruptReceived = True
 
-def receivedFromServer():
-    print("Here!")
 
 def generateNumbers():
     factorA = random.randrange(1, 8)
@@ -24,44 +21,63 @@ def generateNumbers():
     return factors
 
 
-serverIPAddress = ""
-portNumber = 0
-
-clientSocket = socket.socket()
-
-clientInput = ""
-isConnected = False
-
-try:
-    clientSocket.connect((serverIPAddress, portNumber))
-
-except socket.error as clientError:
-    print("Error: Could not connect to server. Exiting now")
-
-
-isConnected = True  # FOR TESTING
-
-if isConnected:
-    valueIndex = 0
-    calculatedValue = 0
-    factorsByIndex = []
-
-    signal.signal(signal.SIGINT, interruptHandler)
+def sendFactors():
+    global clientSocket
+    global factorsByIndex
+    global valueIndex
 
     while not interruptReceived:
-        factors = generateNumbers()
-        factorsByIndex.append(factors)
+        currentFactors = generateNumbers()
+        factorsByIndex.append(currentFactors)
 
-        calculatedValue = (2 ** factors[0]) * (3 ** factors[1]) * (5 ** factors[2]) * (7 ** factors[3])
-        print("{}, {}".format(valueIndex, calculatedValue))
+        calculatedValue = ((2 ** currentFactors[0]) * (3 ** currentFactors[1]) * (5 ** currentFactors[2])
+                           * (7 ** currentFactors[3]))
 
-        
-
-        # send to server
-        # when server 
+        print("Client: Sending Index ({}) and Value ({})\n".format(valueIndex, calculatedValue))
 
         time.sleep(0.5)
         valueIndex += 1
 
-    print("\nInterrupt received. Exiting now")
-        
+
+def validateFactors():
+    global clientSocket
+    global factorsByIndex
+    global valueIndex
+
+    while not interruptReceived:
+        print("Server: Index ({}) and Factors {} are correct.\n".format(valueIndex, factorsByIndex[valueIndex]))
+        time.sleep(0.5)
+
+
+if __name__ == '__main__':
+    interruptReceived = False
+
+    serverIPAddress = ""
+    portNumber = 0
+
+    clientSocket = socket.socket()
+    isConnected = False
+
+    try:
+        clientSocket.connect((serverIPAddress, portNumber))
+        isConnected = True
+    except socket.error as clientError:
+        print("Error: Could not connect to server. Exiting now")
+
+    isConnected = True  # FOR TESTING
+
+    if isConnected:
+        signal.signal(signal.SIGINT, signalHandler)
+        factorsByIndex = []
+        valueIndex = 0
+
+        senderThread = threading.Thread(target=sendFactors)
+        receiverThread = threading.Thread(target=validateFactors)
+
+        senderThread.start()
+        receiverThread.start()
+
+        senderThread.join()
+        receiverThread.join()
+
+        print("\nInterrupt received. Exiting now")
